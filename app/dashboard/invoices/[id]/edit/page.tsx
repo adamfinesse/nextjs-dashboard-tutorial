af705b21744ef77b4f92468caa9674e58462245e
Adam Harms <51472213+adamfinesse@users.noreply.github.com>
2025-12-14 16:24:51 -0500

    metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-import { notFound } from 'next/navigation';
+import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data';
+import {notFound} from 'next/navigation';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -17,7 +22,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {label: 'Invoices', href: '/dashboard/invoices'},
                     {
                         label: 'Edit Invoice',
                         href: `/dashboard/invoices/${id}/edit`,
@@ -25,7 +30,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
                     },
                 ]}
             />
-            <Form invoice={invoice} customers={customers} />
+            <Form invoice={invoice} customers={customers}/>
         </main>
     );
 }
